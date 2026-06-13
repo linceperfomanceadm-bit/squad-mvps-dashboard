86a5304764aa8f618a70a2a7c280f7c1490923c0
henriquefox00-spec <henriquefox00@gmail.com>
2026-06-12 23:00:05 -0300

    Update TaskKanban.js

--- src/components/kanban/TaskKanban.js
+++ src/components/kanban/TaskKanban.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { Plus } from 'lucide-react';
 import { TASK_COLUMNS } from '../../lib/firebase';
 import TaskCard from './TaskCard';
